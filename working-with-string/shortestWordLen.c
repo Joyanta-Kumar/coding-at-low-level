8739812815c00e344ddb369b17b0108ddbfb57fa
Joyanta Kumar <joyroy122004@gmail.com>
2025-08-04 11:52:30 +0600

    Morning, well late morning coding

---
+++ working-with-string/shortestWordLen.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+
+ssize_t find_short(const char *s)
+{
+  size_t smallest = 256;
+  size_t letterCount = 0;
+  for (size_t i = 0; 1; i++) {
+    if (s[i] == ' ' || s[i] == '\0') {
+      if (letterCount < smallest) { smallest = letterCount; }
+      if (s[i] == '\0') { break; }
+      letterCount = 0;
+    }
+    else { letterCount++; }
+  }
+  return smallest;
+}
+
+
+int main()
+{
+  printf("%lu\n", find_short("Let's travel abroad shall we"));
+  return 0;
+}
